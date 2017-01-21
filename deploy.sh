327085254fada6096b7eab2510549f0e8b5a7fe9
Domenic Denicola <d@domenic.me>
2017-01-21 14:52:13 -0500

    Update to newer deploy script
    This one will let us track changes in the build output over time

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,55 @@
 #!/bin/bash
-set -e # exit with nonzero exit code if anything fails
+set -e # Exit with nonzero exit code if anything fails
 
-# Put stuff in the out directory
-npm run build
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
 
-# go to the out directory and create a *new* Git repo
+function doCompile {
+  npm run build
+}
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+TOKEN_REPO=${REPO/https:\/\/github.com/https://${GH_TOKEN}@github.com}
+SHA=`git rev-parse --verify HEAD`
+
+echo $TOKEN_REPO
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO out
 cd out
-git init
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
 
-# inside this git repo we'll pretend to be a new user
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+# Run our compile script
+doCompile
+
+# Now let's go have some fun with the cloned repo
+cd out
 git config user.name "Travis CI"
-git config user.email "d@domenic.me"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+# Now that we're all set up, we can push.
+git push $TOKEN_REPO $TARGET_BRANCH
