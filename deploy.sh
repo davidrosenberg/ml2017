a02652ce50e259d9f810e1515e521fd55a5d9a32
Domenic Denicola <d@domenic.me>
2017-08-14 23:00:03 -0400

    More deploy script tweaks

--- deploy.sh
+++ deploy.sh
@@ -40,14 +40,14 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-# if git diff --quiet; then
-#     echo "No changes to the output on this push; exiting."
-#     exit 0
-# fi
+if [[ -z "$(git status --porcelain)" ]]; then
+  echo "No changes to the output on this push; exiting."
+  exit 0
+fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add -A .
+git add --all .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
@@ -55,8 +55,8 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
